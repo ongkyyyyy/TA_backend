1992a60b616b68ecd2778f001992947065de9264
William Ongky Wijaya <william.ongkywow@gmail.com>
2025-06-21 17:37:59 +0700

    perbaikin config.py

--- config.py
+++ config.py
@@ -1,6 +1,2 @@
-# MONGO_URI = "mongodb+srv://williamongkywow:williamongkywow@clusterta.poz3g.mongodb.net/hotelPerformance"
-# SECRET_KEY = "supersecretkey_987654321"
-import os
-
-MONGO_URI = os.environ.get("MONGODB_URI")
-SECRET_KEY = os.environ.get("SECRET_KEY")
+MONGO_URI = "mongodb+srv://williamongkywow:williamongkywow@clusterta.poz3g.mongodb.net/hotelPerformance"
+SECRET_KEY = "supersecretkey_987654321"
